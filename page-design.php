f2642576b8206e291119a885b2e98b4527b5ba21
resaamiftari <resqmiftari4@icloud.com>
2026-03-11 10:58:33 -0700

    .

---
+++ page-design.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+
+
+
+<div  class="container my-5">
+    <h1 class="text-center mb-5">Css Design Effects</h1>
+
+
+    <!-- Slider -->
+
+
+    <div class="slider">
+        <div class="slides">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide1.jpg">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide2.jpg">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/slide3.jpg">
+
+
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-4">
+            <div class="rounded-box">
+                Rounded Corners
+            </div>
+        </div>
+
+
+        <div class="col-md-4">
+            <div class="rounded-image-box">
+                border Image Example
+            </div>
+        </div>
+         <div class="col-md-4">
+            <div class="background-box">
+                Rounded Corners
+            </div>
+        </div>
+    </div>
+
+
+       
+    </div>
+    </div>
+
+
+
+
+
+
+
+</div>
+
+
+
+<?php get_footer(); ?>
